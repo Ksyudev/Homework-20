a3a1da46e1377105e83c4590e8131fbd5fd94ba5
Ksyudev <92163392+Ksyudev@users.noreply.github.com>
2021-11-30 17:11:22 +0200

    add onsubmit

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,15 +7,17 @@ console.log(`secret ${secret}`);
 let count=10;
 function chekCount() {
     count--;
-    if (count<1) {return newGame()}
-    else {return checkNumber;}
-}
-
+    if (count<=1) 
+    {return newGame()}
+    let attemptsOutput= document.getElementById('attemptsOutput')
+    attemptsOutput.innerHTML= `${+count}`;}
+    
 function checkNumber()
 
-{   let tryNumber = document.getElementById('numberInput').value; 
+{   chekCount();
+    let tryNumber = document.getElementById('numberInput').value; 
     let result = document.getElementById('advicePlace');
-
+    
 
     if (secret==tryNumber)
     {result.innerHTML = 'Вы угадали!!!';
@@ -27,7 +29,7 @@ function checkNumber()
 }
 
 function newGame() {
-
-{advicePlace.innerHTML = 'Игра окончена.';}
+    let result = document.getElementById('advicePlace');
+    result = 'Игра окончена.';
 }
 
