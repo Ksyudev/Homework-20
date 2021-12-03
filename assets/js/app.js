d4a0a88871315fc0b1eace3f4a6fef9807518385
Ksyudev <92163392+Ksyudev@users.noreply.github.com>
2021-12-03 19:03:27 +0200

    edit counter condition

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,48 @@
 
-let secret = Math.floor((Math.random() * 101));
+let secret = Math.ceil((Math.random() * 100));
 console.log(`secret ${secret}`);
-
+    
 
 
 let count=10;
 function chekCount() {
     count--;
-    if (count<=1) 
-    {return newGame()}
-    let attemptsOutput= document.getElementById('attemptsOutput')
-    attemptsOutput.innerHTML= `${+count}`;}
-    
+    let attemptsOutput= document.getElementById('attemptsOutput');  
+    attemptsOutput.innerHTML= `${count}`;
+}
+  
+
 function checkNumber()
 
-{   chekCount();
+{   
     let tryNumber = document.getElementById('numberInput').value; 
     let result = document.getElementById('advicePlace');
-    
-
-    if (secret==tryNumber)
-    {result.innerHTML = 'Вы угадали!!!';
-    return;}
-    else if (secret>tryNumber)
-        {result.innerHTML = 'Мое число больше вашего.';}
-    else
-        {result.innerHTML = 'Мое число меньше вашего.';} 
+    chekCount();
+    if (count>=1) 
+    {
+        {if (secret==tryNumber)
+        {result.innerHTML = 'Вы угадали!!!';
+        attemptsOutput.innerHTML= `Поздравляю!`;
+        return;}
+        else if (secret>tryNumber)
+            {result.innerHTML = 'Мое число больше вашего.';}
+        else
+            {result.innerHTML = 'Мое число меньше вашего.';} 
+        } 
+        document.getElementById('numberInput').value='';
+         console.log(`count ${count}`);
+    }
+    else if (count<=0)    
+    {       if (secret==tryNumber)
+            {result.innerHTML = 'Вы угадали!!!';
+            attemptsOutput.innerHTML= `Поздравляю!`;
+            return;}
+        else 
+            {attemptsOutput.innerHTML= `Игра окончена`;
+             result.innerHTML = '';
+             document.getElementById('numberInput').value='';
+             return;}          
+    }  
 }
 
-function newGame() {
-    let result = document.getElementById('advicePlace');
-    result = 'Игра окончена.';
-}
 
